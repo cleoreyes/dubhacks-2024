812784c60ac53fcda30e28e72f2667db5c00a37e
Keira <1keira.kw@gmail.com>
2024-10-13 00:52:06 -0700

    Added a scrapbook feature on the home page to make it more personal and customizable for the user

--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
+import heartImage from "../img/heart.png";
 
 const Homepage = () => {
+  const [position, setPosition] = useState({ x: 20, y: 20 });
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+  const heartRef = useRef(null);
+
+  const handleMouseDown = (e) => {
+    if (heartRef.current && heartRef.current.contains(e.target)) {
+      setIsDragging(true);
+      const rect = heartRef.current.getBoundingClientRect();
+      setDragOffset({
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+      });
+    }
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      setPosition({
+        x: e.clientX - dragOffset.x,
+        y: e.clientY - dragOffset.y,
+      });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
   return (
-    <>
+    <div
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      className="position-relative vh-100"
+    >
       <Navbar />
       <div className="vh-100">
         <h1 className="text-center pb-2 pt-4">
@@ -66,7 +102,21 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-    </>
+      <img
+        ref={heartRef}
+        src={heartImage}
+        alt="Draggable Heart"
+        style={{
+          position: 'absolute',
+          left: `${position.x}px`,
+          top: `${position.y}px`,
+          cursor: 'move',
+          userSelect: 'none',
+          width: '100px',
+          height: 'auto',
+        }}
+      />
+    </div>
   );
 };
 
