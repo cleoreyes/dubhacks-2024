6316592bd0eb13f0a5874f0aad165bf34f539229
Keira <1keira.kw@gmail.com>
2024-10-12 20:08:17 -0700

    Created a Navbar.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HomePage from './pages/HomePage';
 import SecurityQuestPage from './pages/SecurityQuestPage';
+import Navbar from './pages/Navbar';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'; // Your own custom styles
@@ -8,7 +9,7 @@ import './index.css'; // Your own custom styles
 function App() {
   return (
     <div className="App">
-      <SecurityQuestPage />
+      <Navbar />
 
     </div>
   );
---
+++ my-app/src/pages/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Navbar = () => {
+  const navbarStyle = {
+    backgroundColor: '#91A3B0',
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  };
+
+  const brandStyle = {
+    fontSize: '1.25rem',
+    fontWeight: 'bold',
+    color: '#000',
+    textDecoration: 'none',
+  };
+
+  const navListStyle = {
+    listStyle: 'none',
+    display: 'flex',
+    padding: 0,
+    margin: 0,
+  };
+
+  const navItemStyle = {
+    margin: '0 0.5rem',
+  };
+
+  const navLinkStyle = {
+    color: '#000',
+    textDecoration: 'none',
+  };
+
+  return (
+    <nav style={navbarStyle}>
+      <a href="#" style={brandStyle}>Eterna</a>
+      <ul style={navListStyle}>
+        {/* <li style={navItemStyle}><img src="img/circle.png"></img><a href="#profile" style={navLinkStyle}>Profile</a></li> */}
+        <a href="#profile"><button className="circle-button" style={{ borderRadius: '100%' }}>O</button></a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
