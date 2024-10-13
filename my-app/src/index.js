fd21ca3304ae9d09ce3613e2b7ad73a008bf5bb1
Derek <107140943+Deleted-0970@users.noreply.github.com>
2024-10-12 18:54:22 -0700

    store video changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,22 @@
+import { initializeApp } from "firebase/app";
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJji4Y3Uiwf7gqvessKHBtCTSLv2Nvhio",
+  authDomain: "eternadb-0.firebaseapp.com",
+  projectId: "eternadb-0",
+  storageBucket: "eternadb-0.appspot.com",
+  messagingSenderId: "245121678872",
+  appId: "1:245121678872:web:d2a4fde2825f624b90cd6d"
+};
+
+initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
