5a772b2100c5c4f35b718810fddba345d90d86e4
Keira <1keira.kw@gmail.com>
2024-10-12 19:07:02 -0700

    Added security question page components including the question, textbox, and submit button. Submit button needs routing to Home Page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import SecurityQuestPage from './pages/SecurityQuestPage';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'; // Your own custom styles
@@ -7,8 +8,8 @@ import './index.css'; // Your own custom styles
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      
+      <SecurityQuestPage />
+
     </div>
   );
 }
